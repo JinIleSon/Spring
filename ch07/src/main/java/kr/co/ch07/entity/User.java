0e81b7c4e8dbf51e4721f2817228c5589b0da571
thswlsdlf0000 <sy7521012@naver.com>
2025-09-23 12:38:07 +0900

    ch07 진행중

---
+++ ch07/src/main/java/kr/co/ch07/entity/User.java
@@ -0,0 +1,30 @@
+package kr.co.ch07.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "SC_USER")
+public class User {
+
+    @Id
+    private String usId;
+    private String pass;
+    private String name;
+    private int age;
+    private String role;
+
+    @CreationTimestamp
+    private LocalDateTime regDate;
+
+}
